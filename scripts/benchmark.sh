411e88a7cbadaddf8613de8d4c867c68964090a3
Akshay Subramaniam <asubramaniam@nvidia.com>
2021-08-27 15:15:15 -0700

    Updated script to use tmp directory and a trap to clean up

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,14 +7,14 @@ if [ $# -lt 2 ]
     exit 1
 fi
 
-ALGO=$1
-DIR=$2
+ALGO="$1"
+DIR="$2"
 
 BINARY="../build/bin/benchmark_${ALGO}_chunked"
 
-# Create a subdirectory for all the logs
-LOGDIR="./benchmark_logs"
-mkdir -p ${LOGDIR}
+# Create a temp directory for all the logs
+LOGDIR="$(mktemp -d)"
+trap 'rm -rf -- "${LOGDIR}"' EXIT
 
 output_header() {
   echo dataset, uncompressed bytes, compression ratio, "compression throughput (GB/s)", "decompression throughput (GB/s)"
@@ -24,12 +24,11 @@ output_header() {
 run_benchmark () {
   CMD="$1 -f $2"
   FILENAME="${LOGDIR}/$(basename $1)_$(basename $2).log"
-  # echo "${CMD} &> ${FILENAME}"
-  ${CMD} &> ${FILENAME}
-  bytes=$(awk '/^uncompressed /{print $3}' ${FILENAME})
-  ratio=$(awk '/compressed ratio:/{print $5}' ${FILENAME})
-  comp_throughput=$(awk '/^compression throughput /{print $4}' ${FILENAME})
-  decomp_throughput=$(awk '/^decompression throughput /{print $4}' ${FILENAME})
+  ${CMD} &> "${FILENAME}"
+  bytes=$(awk '/^uncompressed /{print $3}' "${FILENAME}")
+  ratio=$(awk '/compressed ratio:/{print $5}' "${FILENAME}")
+  comp_throughput=$(awk '/^compression throughput /{print $4}' "${FILENAME}")
+  decomp_throughput=$(awk '/^decompression throughput /{print $4}' "${FILENAME}")
   echo $(basename $2), $bytes, $ratio, $comp_throughput, $decomp_throughput
   return 0
 }
@@ -38,11 +37,8 @@ run_benchmark () {
 output_header
 for fname in ${DIR}/*
 do
-  if [[ -f $fname ]]
+  if [[ -f "${fname}" ]]
   then
-    run_benchmark $BINARY $fname
+    run_benchmark "$BINARY" "$fname"
   fi
 done
-
-# Delete all the log files
-rm -rf ${LOGDIR}
