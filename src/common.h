fe92de4b43c01c5cc7289fdc5edbf59d71dd2b02
Eric Schmidt <eschmidt@nvidia.com>
2021-12-29 14:33:23 -0800

    Add a common error checking method for cuda API errors

--- src/common.h
+++ src/common.h
@@ -187,4 +187,14 @@ __inline__ size_t sizeOfnvcompType(nvcompType_t type)
   }
 }
 
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); } 
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 } // namespace nvcomp
