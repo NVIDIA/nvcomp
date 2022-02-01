bd28a516f4bf07e986d10063922dab88ba8ec98a
Eric Schmidt <eschmidt@nvidia.com>
2022-02-01 12:30:09 -0800

    Reorg to allow run-averaging in hlif benchmarks

--- benchmarks/benchmark_hlif.cpp
+++ benchmarks/benchmark_hlif.cpp
@@ -33,137 +33,18 @@
 #include "benchmark_common.h"
 #include "nvcomp.hpp"
 #include "nvcomp/nvcompManagerFactory.hpp"
+#include "benchmark_hlif.hpp"
 
 using namespace nvcomp;
 
-template<typename T = uint8_t>
-void run_benchmark(const std::vector<T>& data, nvcompManagerBase& batch_manager, int verbose_memory, cudaStream_t stream)
-{
-  size_t input_element_count = data.size();
-
-  // Make sure dataset fits on GPU to benchmark total compression
-  size_t freeMem;
-  size_t totalMem;
-  CUDA_CHECK(cudaMemGetInfo(&freeMem, &totalMem));
-  if (freeMem < input_element_count * sizeof(T)) {
-    std::cout << "Insufficient GPU memory to perform compression." << std::endl;
-    exit(1);
-  }
-  
-  std::cout << "----------" << std::endl;
-  std::cout << "uncompressed (B): " << data.size() * sizeof(T) << std::endl;
-
-  T* d_in_data;
-  const size_t in_bytes = sizeof(T) * input_element_count;
-  CUDA_CHECK(cudaMalloc((void**)&d_in_data, in_bytes));
-  CUDA_CHECK(
-      cudaMemcpy(d_in_data, data.data(), in_bytes, cudaMemcpyHostToDevice));
-
-  auto compress_config = batch_manager.configure_compression(in_bytes);
-  
-  size_t comp_out_bytes = compress_config.max_compressed_buffer_size;
-  benchmark_assert(
-      comp_out_bytes > 0, "Output size must be greater than zero.");
-
-  // Allocate temp workspace
-  size_t comp_scratch_bytes = batch_manager.get_required_scratch_buffer_size();
-  uint8_t* d_comp_scratch;
-  CUDA_CHECK(cudaMalloc(&d_comp_scratch, comp_scratch_bytes));
-  batch_manager.set_scratch_buffer(d_comp_scratch);
-
-  // Allocate compressed output buffer
-  uint8_t* d_comp_out;
-  CUDA_CHECK(cudaMalloc(&d_comp_out, comp_out_bytes));
-
-  if (verbose_memory) {
-    std::cout << "compression memory (input+output+scratch) (B): "
-              << (in_bytes + comp_out_bytes + comp_scratch_bytes) << std::endl;
-    std::cout << "compression scratch space (B): " << comp_scratch_bytes << std::endl;
-    std::cout << "compression output space (B): " << comp_out_bytes
-              << std::endl;
-  }
-
-  // Launch compression
-  auto start = std::chrono::steady_clock::now();
-  batch_manager.compress(
-      d_in_data,
-      d_comp_out,
-      compress_config);
-  CUDA_CHECK(cudaStreamSynchronize(stream));
-  auto end = std::chrono::steady_clock::now();
-  comp_out_bytes = batch_manager.get_compressed_output_size(d_comp_out);
-
-  CUDA_CHECK(cudaFree(d_comp_scratch));
-  CUDA_CHECK(cudaFree(d_in_data));
-
-  std::cout << "comp_size: " << comp_out_bytes
-            << ", compressed ratio: " << std::fixed << std::setprecision(2)
-            << (double)data.size() * sizeof(T) / comp_out_bytes << std::endl;
-  std::cout << "compression throughput (GB/s): "
-            << gbs(start, end, data.size() * sizeof(T)) << std::endl;
-
-  // allocate output buffer
-  auto decomp_config = batch_manager.configure_decompression(d_comp_out);
-  const size_t decomp_bytes = decomp_config.decomp_data_size;
-  uint8_t* decomp_out_ptr;
-  CUDA_CHECK(cudaMalloc(&decomp_out_ptr, decomp_bytes));
-  const size_t decomp_temp_bytes = 0;
-
-  // get output size
-  if (verbose_memory) {
-    std::cout << "decompression memory (input+output+temp) (B): "
-              << (decomp_bytes + comp_out_bytes + decomp_temp_bytes)
-              << std::endl;
-    std::cout << "decompression temp space (B): " << decomp_temp_bytes
-              << std::endl;
-  }
-
-  start = std::chrono::steady_clock::now();
-
-  // execute decompression (asynchronous)
-  batch_manager.decompress(decomp_out_ptr, d_comp_out, decomp_config);
-
-  CUDA_CHECK(cudaStreamSynchronize(stream));
-  end = std::chrono::steady_clock::now();
-
-  std::cout << "decompression throughput (GB/s): "
-            << gbs(start, end, decomp_bytes) << std::endl;
-
-  CUDA_CHECK(cudaFree(d_comp_out));
-
-  benchmark_assert(
-      decomp_bytes == input_element_count * sizeof(T),
-      "Decompressed result incorrect size.");
-
-  std::vector<T> res(input_element_count);
-  cudaMemcpy(
-      res.data(),
-      decomp_out_ptr,
-      input_element_count * sizeof(T),
-      cudaMemcpyDeviceToHost);
-  
-  CUDA_CHECK(cudaFree(decomp_out_ptr));
-  
-  // check the size
-#if VERBOSE > 1
-  // dump output data
-  std::cout << "Output" << std::endl;
-  for (size_t i = 0; i < data.size(); i++)
-    std::cout << ((T*)out_ptr)[i] << " ";
-  std::cout << std::endl;
-#endif
-
-  benchmark_assert(res == data, "Decompressed data does not match input.");
-}
-
-void run_benchmark_from_file(char* fname, nvcompManagerBase& batch_manager, int verbose_memory, cudaStream_t stream)
+void run_benchmark_from_file(char* fname, nvcompManagerBase& batch_manager, int verbose_memory, cudaStream_t stream, const int benchmark_exec_count)
 {
   using T = uint8_t;
 
   size_t input_elts = 0;
   std::vector<T> data;
   data = load_dataset_from_binary<T>(fname, &input_elts);
-  run_benchmark(data, batch_manager, verbose_memory, stream);
+  run_benchmark(data, batch_manager, verbose_memory, stream, benchmark_exec_count);
 }
 
 static void print_usage()
@@ -173,6 +54,7 @@ static void print_usage()
   printf("  %-35s Binary dataset filename (required).\n", "-f, --filename");
   printf("  %-35s Chunk size (default 64 kB).\n", "-c, --chunk-size");
   printf("  %-35s GPU device number (default 0)\n", "-g, --gpu");
+  printf("  %-35s Number of times to execute the benchmark (for averaging) (default 1)\n", "-n, --num-iters");
   printf("  %-35s Data type (default 'char', options are 'char', 'short', 'int')\n", "-t, --type");
   printf(
       "  %-35s Output GPU memory allocation sizes (default off)\n",
@@ -185,6 +67,7 @@ int main(int argc, char* argv[])
   char* fname = NULL;
   int gpu_num = 0;
   int verbose_memory = 0;
+  int num_iters = 1;
 
   // Cascaded opts
   nvcompBatchedCascadedOpts_t cascaded_opts = nvcompBatchedCascadedDefaultOpts;
@@ -227,6 +110,7 @@ int main(int argc, char* argv[])
       continue;
     }
 
+
     // all arguments below require at least a second value in argv
     if (argv >= argv_end) {
       print_usage();
@@ -238,11 +122,16 @@ int main(int argc, char* argv[])
       fname = optarg;
       continue;
     }
+    
     if (strcmp(arg, "--gpu") == 0 || strcmp(arg, "-g") == 0) {
       gpu_num = atoi(optarg);
       continue;
     }
 
+    if (strcmp(arg, "--num-iters") == 0 || strcmp(arg, "-n") == 0) {
+      num_iters = atoi(optarg);
+      continue;
+    }
     if (strcmp(arg, "--chunk-size") == 0 || strcmp(arg, "-c") == 0) {
       chunk_size = atoi(optarg);
       explicit_chunk_size = true;
@@ -319,7 +208,7 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  run_benchmark_from_file(fname, *manager, verbose_memory, stream);
+  run_benchmark_from_file(fname, *manager, verbose_memory, stream, num_iters);
   CUDA_CHECK(cudaStreamDestroy(stream));
 
   return 0;
