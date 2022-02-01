04178561172204c05a8c88e479a7a35ae64cabbf
Eric Schmidt <eschmidt@nvidia.com>
2022-01-31 17:34:50 -0800

    Add more examples to high_level_quickstart_example

--- examples/high_level_quickstart_example.cpp
+++ examples/high_level_quickstart_example.cpp
@@ -47,21 +47,37 @@
 
 using namespace nvcomp;
 
-void execute_example(uint8_t* device_input_data, const size_t in_bytes)
+#define CUDA_CHECK(cond)                                                       \
+  do {                                                                         \
+    cudaError_t err = cond;                                                    \
+    if (err != cudaSuccess) {                                               \
+      std::cerr << "Failure" << std::endl;                                \
+      exit(1);                                                              \
+    }                                                                         \
+  } while (false)
+
+/**
+ * In this example, we 
+ * 
+ *  1) compress the input data
+ *  2) construct a new manager using the input data for demonstration purposes
+ *  3) decompress the input data
+ */ 
+void decomp_compressed_with_manager_factory_example(uint8_t* device_input_ptrs, const size_t input_buffer_len)
 {
   cudaStream_t stream;
-  cudaStreamCreate(&stream);
+  CUDA_CHECK(cudaStreamCreate(&stream));
 
   const int chunk_size = 1 << 16;
   nvcompType_t data_type = NVCOMP_TYPE_CHAR;
 
   LZ4Manager nvcomp_manager{chunk_size, data_type, stream};
-  CompressionConfig comp_config = nvcomp_manager.configure_compression(in_bytes);
+  CompressionConfig comp_config = nvcomp_manager.configure_compression(input_buffer_len);
 
   uint8_t* comp_buffer;
-  cudaMalloc(&comp_buffer, comp_config.max_compressed_buffer_size);
+  CUDA_CHECK(cudaMalloc(&comp_buffer, comp_config.max_compressed_buffer_size));
   
-  nvcomp_manager.compress(device_input_data, comp_buffer, comp_config);
+  nvcomp_manager.compress(device_input_ptrs, comp_buffer, comp_config);
 
   // Construct a new nvcomp manager from the compressed buffer.
   // Note we could use the nvcomp_manager from above, but here we demonstrate how to create a manager 
@@ -72,20 +88,151 @@ void execute_example(uint8_t* device_input_data, const size_t in_bytes)
 
   DecompressionConfig decomp_config = decomp_nvcomp_manager->configure_decompression(comp_buffer);
   uint8_t* res_decomp_buffer;
-  cudaMalloc(&res_decomp_buffer, decomp_config.decomp_data_size);
+  CUDA_CHECK(cudaMalloc(&res_decomp_buffer, decomp_config.decomp_data_size));
 
   decomp_nvcomp_manager->decompress(res_decomp_buffer, comp_buffer, decomp_config);
 
-  cudaStreamSynchronize(stream);
+  CUDA_CHECK(cudaStreamSynchronize(stream));
 
-  cudaStreamDestroy(stream);
+  CUDA_CHECK(cudaStreamDestroy(stream));
+}
+
+/**
+ * Or, we can use the same manager for both compression and decompression.
+ * 
+ * In this example, we 
+ * 
+ *  0) construct an nvcompManager
+ *  1) compress the input data
+ *  2) decompress the input data
+ */ 
+void comp_decomp_with_single_manager(uint8_t* device_input_ptrs, const size_t input_buffer_len)
+{
+  cudaStream_t stream;
+  CUDA_CHECK(cudaStreamCreate(&stream));
+
+  const int chunk_size = 1 << 16;
+  nvcompType_t data_type = NVCOMP_TYPE_CHAR;
+
+  LZ4Manager nvcomp_manager{chunk_size, data_type, stream};
+  CompressionConfig comp_config = nvcomp_manager.configure_compression(input_buffer_len);
+
+  uint8_t* comp_buffer;
+  CUDA_CHECK(cudaMalloc(&comp_buffer, comp_config.max_compressed_buffer_size));
+  
+  nvcomp_manager.compress(device_input_ptrs, comp_buffer, comp_config);
+
+  DecompressionConfig decomp_config = nvcomp_manager.configure_decompression(comp_buffer);
+  uint8_t* res_decomp_buffer;
+  CUDA_CHECK(cudaMalloc(&res_decomp_buffer, decomp_config.decomp_data_size));
+
+  nvcomp_manager.decompress(res_decomp_buffer, comp_buffer, decomp_config);
+
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+
+  CUDA_CHECK(cudaStreamDestroy(stream));
+}
+
+/**
+ * Additionally, we can use the same manager to execute multiple streamed compressions / decompressions
+ * In this example we configure the multiple decompressions by inspecting the compressed buffers
+ */  
+void multi_comp_decomp_example(const std::vector<uint8_t*>& device_input_ptrs, std::vector<size_t>& input_buffer_lengths)
+{
+  size_t num_buffers = input_buffer_lengths.size();
+  
+  using namespace std;
+
+  cudaStream_t stream;
+  CUDA_CHECK(cudaStreamCreate(&stream));
+
+  const int chunk_size = 1 << 16;
+  nvcompType_t data_type = NVCOMP_TYPE_CHAR;
+
+  LZ4Manager nvcomp_manager{chunk_size, data_type, stream};
+  
+  std::vector<uint8_t*> comp_result_buffers(num_buffers);
+
+  for(size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    uint8_t* input_data = device_input_ptrs[ix_buffer];
+    size_t input_length = input_buffer_lengths[ix_buffer];
+
+    auto comp_config = nvcomp_manager.configure_compression(input_length);
+
+    CUDA_CHECK(cudaMalloc(&comp_result_buffers[ix_buffer], comp_config.max_compressed_buffer_size));
+    nvcomp_manager.compress(input_data, comp_result_buffers[ix_buffer], comp_config);    
+  }
+
+  std::vector<uint8_t*> decomp_result_buffers(num_buffers);
+  for(size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    uint8_t* comp_data = comp_result_buffers[ix_buffer];
+
+    auto decomp_config = nvcomp_manager.configure_decompression(comp_data);
+
+    CUDA_CHECK(cudaMalloc(&decomp_result_buffers[ix_buffer], decomp_config.decomp_data_size));
+
+    nvcomp_manager.decompress(decomp_result_buffers[ix_buffer], comp_data, decomp_config);    
+  }
+
+  for (size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    CUDA_CHECK(cudaFree(decomp_result_buffers[ix_buffer]));
+    CUDA_CHECK(cudaFree(comp_result_buffers[ix_buffer]));
+  }
+}
+
+/**
+ * Additionally, we can use the same manager to execute multiple streamed compressions / decompressions
+ * In this example we configure the multiple decompressions by storing the comp_config's and inspecting those
+ */  
+void multi_comp_decomp_example_comp_config(const std::vector<uint8_t*>& device_input_ptrs, std::vector<size_t>& input_buffer_lengths)
+{
+  size_t num_buffers = input_buffer_lengths.size();
+
+  cudaStream_t stream;
+  CUDA_CHECK(cudaStreamCreate(&stream));
+
+  const int chunk_size = 1 << 16;
+  nvcompType_t data_type = NVCOMP_TYPE_CHAR;
+
+  LZ4Manager nvcomp_manager{chunk_size, data_type, stream};
+  
+  std::vector<CompressionConfig> comp_configs;
+  comp_configs.reserve(num_buffers);
+
+  std::vector<uint8_t*> comp_result_buffers(num_buffers);
+
+  for(size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    uint8_t* input_data = device_input_ptrs[ix_buffer];
+    size_t input_length = input_buffer_lengths[ix_buffer];
+
+    comp_configs.push_back(nvcomp_manager.configure_compression(input_length));
+    auto& comp_config = comp_configs.back();
+
+    CUDA_CHECK(cudaMalloc(&comp_result_buffers[ix_buffer], comp_config.max_compressed_buffer_size));
+
+    nvcomp_manager.compress(input_data, comp_result_buffers[ix_buffer], comp_config);    
+  }
+
+  std::vector<uint8_t*> decomp_result_buffers(num_buffers);
+  for(size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    auto decomp_config = nvcomp_manager.configure_decompression(comp_configs[ix_buffer]);
+
+    CUDA_CHECK(cudaMalloc(&decomp_result_buffers[ix_buffer], decomp_config.decomp_data_size));
+
+    nvcomp_manager.decompress(decomp_result_buffers[ix_buffer], comp_result_buffers[ix_buffer], decomp_config);    
+  }
+
+  for (size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    CUDA_CHECK(cudaFree(decomp_result_buffers[ix_buffer]));
+    CUDA_CHECK(cudaFree(comp_result_buffers[ix_buffer]));
+  }
 }
 
 int main()
 {
   // Initialize a random array of chars
-  const size_t in_bytes = 1000000;
-  std::vector<uint8_t> uncompressed_data(in_bytes);
+  const size_t input_buffer_len = 1000000;
+  std::vector<uint8_t> uncompressed_data(input_buffer_len);
   
   std::mt19937 random_gen(42);
 
@@ -93,17 +240,42 @@ int main()
   // non-standard, and isn't available on MSVC, so use short instead,
   // but with the range limited, and then cast below.
   std::uniform_int_distribution<short> uniform_dist(0, 255);
-  for (size_t ix = 0; ix < in_bytes; ++ix) {
+  for (size_t ix = 0; ix < input_buffer_len; ++ix) {
     uncompressed_data[ix] = static_cast<uint8_t>(uniform_dist(random_gen));
   }
 
-  uint8_t* device_input_data;
-  cudaMalloc((void**)&device_input_data, in_bytes);
-  cudaMemcpy(device_input_data, uncompressed_data.data(), in_bytes, cudaMemcpyDefault);
+  uint8_t* device_input_ptrs;
+  CUDA_CHECK(cudaMalloc(&device_input_ptrs, input_buffer_len));
+  CUDA_CHECK(cudaMemcpy(device_input_ptrs, uncompressed_data.data(), input_buffer_len, cudaMemcpyDefault));
   
-  execute_example(device_input_data, in_bytes);
+  // Two roundtrip examples
+  decomp_compressed_with_manager_factory_example(device_input_ptrs, input_buffer_len);
+  comp_decomp_with_single_manager(device_input_ptrs, input_buffer_len);
 
-  cudaFree(device_input_data);
+  CUDA_CHECK(cudaFree(device_input_ptrs));
 
+  // Multi buffer example
+  const size_t num_buffers = 10;
+
+  std::vector<uint8_t*> gpu_buffers(num_buffers);
+  std::vector<size_t> input_buffer_lengths(num_buffers);
+
+  std::vector<std::vector<uint8_t>> uncompressed_buffers(num_buffers);
+  for (size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    uncompressed_buffers[ix_buffer].resize(input_buffer_len);
+    for (size_t ix_byte = 0; ix_byte < input_buffer_len; ++ix_byte) {
+      uncompressed_buffers[ix_buffer][ix_byte] = static_cast<uint8_t>(uniform_dist(random_gen));
+    }
+    CUDA_CHECK(cudaMalloc(&gpu_buffers[ix_buffer], input_buffer_len));
+    CUDA_CHECK(cudaMemcpy(gpu_buffers[ix_buffer], uncompressed_buffers[ix_buffer].data(), input_buffer_len, cudaMemcpyDefault));
+    input_buffer_lengths[ix_buffer] = input_buffer_len;
+  }
+
+  multi_comp_decomp_example(gpu_buffers, input_buffer_lengths);
+  multi_comp_decomp_example_comp_config(gpu_buffers, input_buffer_lengths);
+
+  for (size_t ix_buffer = 0; ix_buffer < num_buffers; ++ix_buffer) {
+    CUDA_CHECK(cudaFree(gpu_buffers[ix_buffer]));
+  }
   return 0;
 }
